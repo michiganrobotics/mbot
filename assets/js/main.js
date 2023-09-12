0472fca87b2ff29228df6ab532fe2b61253b034a
Shaw <sunxycn98@gmail.com>
2023-09-12 19:22:31 -0400

    bottom plate assemble finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,8 @@ $(document).ready(function() {
         image: {
           tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
           titleSrc: function(item) {
-            return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            // return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            return item.el.attr('title');
           }
         }
       });
